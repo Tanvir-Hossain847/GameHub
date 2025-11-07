5b44aa14891ff0438e6b8bb3b27d643999a9da55
Tanvir-Hossain847 <tanvirhossaintufa@gmail.com>
2025-11-08 02:07:12 +0600

    add profile page add bids and products page

--- src/Pages/AllProducts.jsx
+++ src/Pages/AllProducts.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AllProducts = () => {
     return (
         <div>
-            <div className="flex w-11/12 mx-auto justify-between">
+            <div className="flex w-11/12 mx-auto justify-between my-20">
                             <img className='w-80 rounded-2xl' src="https://i.ibb.co.com/nqdhSNYx/2259129.jpg" alt="" />
                             <img className='w-80 rounded-2xl' src="https://i.ibb.co.com/nqdhSNYx/2259129.jpg" alt="" />
                             <img className='w-80 rounded-2xl' src="https://i.ibb.co.com/nqdhSNYx/2259129.jpg" alt="" />
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -4,6 +4,9 @@ import Home from '../Pages/Home';
 import Registration from '../Pages/Register';
 import Login from '../Pages/Login';
 import Root from '../Layouts/Root';
+import AllProducts from '../Pages/AllProducts';
+import BIds from '../Pages/BIds';
+import Profile from '../Pages/Profile';
 
 export const router = createBrowserRouter([
   {
@@ -18,7 +21,15 @@ export const router = createBrowserRouter([
         },
         {
           path: '/products',
-          element: 
+          element: <AllProducts></AllProducts>
+        },
+        {
+          path: '/bids',
+          element: <BIds></BIds>
+        },
+        {
+          path: '/profile',
+          element: <Profile></Profile>
         },
         {
           path:'/register',
